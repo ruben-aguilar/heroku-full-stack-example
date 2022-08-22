577bd44def7e299f580206bf61430bf6d7d2d56a
Ruben Aguilar <ruben.aguilar.becerra@gmail.com>
2022-08-22 23:28:11 +0200

    Fixing port again

--- backend/server.js
+++ backend/server.js
@@ -16,8 +16,7 @@ const express = require('express')
 const app = express()
 
 // Heroku will populate the PORT environment too
-// const port = process.env.PORT;
-const port = 80;
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
