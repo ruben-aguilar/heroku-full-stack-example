4852497d48e3eeb7c8bbd67b54b92917e33a9c82
Ruben Aguilar <ruben.aguilar.becerra@gmail.com>
2022-08-22 23:45:02 +0200

    Fix typo

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ const app = express();
 // })
 
 
-app.use(express.static(path.join(__dirname, 'public'))
+app.use(express.static(path.join(__dirname, 'public')))
 
 // Heroku will populate the PORT environment too
 
