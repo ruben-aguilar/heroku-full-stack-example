6822cfd05ce41a4daa22693b40f79bf292a3bcaf
Ruben Aguilar <ruben.aguilar.becerra@gmail.com>
2022-08-22 23:00:08 +0200

    first commit

---
+++ backend/server.js
@@ -0,0 +1,35 @@
+const express = require('express')
+
+
+const { Client } = require('pg');
+
+const client = new Client({
+    // Heroku will populate this environment variable
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+client.connect();
+
+const app = express()
+
+// Heroku will populate the PORT environment too
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/message', (req, res) => {
+    client.query('SELECT * from Messages', (err, messages) => {
+        res.send(messages[0])
+    })
+})
+
+
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
