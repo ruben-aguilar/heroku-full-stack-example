75fd3ca7511adcdc1d01d0d2b8f54583e06ffb2d
Ruben Aguilar <ruben.aguilar.becerra@gmail.com>
2022-08-22 23:22:50 +0200

    Moving to root

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,32 @@
 const express = require('express')
 
 
-const { Client } = require('pg');
+// const { Client } = require('pg');
 
-const client = new Client({
-    // Heroku will populate this environment variable
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
+// const client = new Client({
+//     // Heroku will populate this environment variable
+//     connectionString: process.env.DATABASE_URL,
+//     ssl: {
+//         rejectUnauthorized: false
+//     }
+// });
 
-client.connect();
+// client.connect();
 
 const app = express()
 
 // Heroku will populate the PORT environment too
-const port = process.env.PORT;
+// const port = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.get('/message', (req, res) => {
-    client.query('SELECT * from Messages', (err, messages) => {
-        res.send(messages[0])
-    })
-})
+// app.get('/message', (req, res) => {
+//     client.query('SELECT * from Messages', (err, messages) => {
+//         res.send(messages[0])
+//     })
+// })
 
 
 
